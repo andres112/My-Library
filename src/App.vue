ccaf2350f10bfb36d25e2ec214698d4ee3727d1c
Andres Felipe Dorado <andres.dorado90@gmail.com>
2023-08-09 20:39:59 +0200

    Add and configure tailwind (#12)
    * chore: code formating
    
    * feat: add tailwind
    
    * chore: some styles with tailwind added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import BooksIcon from './components/icons/IconBooks.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div class="flex items-center">
+      <div class="mx-3 max-w-[75px] md:me-6 md:max-w-[100px] xl:max-w-[150px]">
+        <BooksIcon />
+      </div>
+      <HelloWorld msg="My library" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
+      <!-- <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-      </nav>
+      </nav> -->
     </div>
   </header>
 
@@ -24,11 +26,6 @@ import HelloWorld from './components/HelloWorld.vue'
 header {
   line-height: 1.5;
   max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 nav {
@@ -62,11 +59,6 @@ nav a:first-of-type {
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
   header .wrapper {
     display: flex;
     place-items: flex-start;
