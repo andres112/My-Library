f824c6b45c0fd3a882f4c7365c8002038745737c
Andres Felipe Dorado <andres.dorado90@gmail.com>
2023-09-02 22:39:07 +0200

    chore: loading animation

--- src/views/edit/AuthorSearchList.vue
+++ src/views/edit/AuthorSearchList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-[90vh] mt-10 divide-y divide-primary/25 overflow-y-auto">
-    <div v-if="store.loading" class="text-2xl">Loading...</div>
+    <div v-if="store.loading" class="loader text-2xl"></div>
     <div v-if="store.authorList.length === 0 && !store.loading" class="text-2xl">
       Author not found
     </div>
@@ -44,3 +44,164 @@ watch(store.authorList, (newVal) => {
   console.log(newVal)
 })
 </script>
+
+<style lang="scss">
+.loader {
+  --w: 10ch;
+  font-weight: bold;
+  line-height: 1.4em;
+  letter-spacing: var(--w);
+  width: var(--w);
+  overflow: hidden;
+  white-space: nowrap;
+  color: white;
+  text-shadow:
+    calc(0 * var(--w)) 0 white,
+    calc(-1 * var(--w)) 0 white,
+    calc(-2 * var(--w)) 0 white,
+    calc(-3 * var(--w)) 0 white,
+    calc(-4 * var(--w)) 0 white,
+    calc(-5 * var(--w)) 0 white,
+    calc(-6 * var(--w)) 0 white,
+    calc(-7 * var(--w)) 0 white,
+    calc(-8 * var(--w)) 0 white,
+    calc(-9 * var(--w)) 0 white;
+  animation: l20 2s infinite linear;
+}
+.loader:before {
+  content: 'Loading...';
+}
+
+@keyframes l20 {
+  9.09% {
+    text-shadow:
+      calc(0 * var(--w)) -10px white,
+      calc(-1 * var(--w)) 0 white,
+      calc(-2 * var(--w)) 0 white,
+      calc(-3 * var(--w)) 0 white,
+      calc(-4 * var(--w)) 0 white,
+      calc(-5 * var(--w)) 0 white,
+      calc(-6 * var(--w)) 0 white,
+      calc(-7 * var(--w)) 0 white,
+      calc(-8 * var(--w)) 0 white,
+      calc(-9 * var(--w)) 0 white;
+  }
+  18.18% {
+    text-shadow:
+      calc(0 * var(--w)) 0 white,
+      calc(-1 * var(--w)) -10px white,
+      calc(-2 * var(--w)) 0 white,
+      calc(-3 * var(--w)) 0 white,
+      calc(-4 * var(--w)) 0 white,
+      calc(-5 * var(--w)) 0 white,
+      calc(-6 * var(--w)) 0 white,
+      calc(-7 * var(--w)) 0 white,
+      calc(-8 * var(--w)) 0 white,
+      calc(-9 * var(--w)) 0 white;
+  }
+  27.27% {
+    text-shadow:
+      calc(0 * var(--w)) 0 white,
+      calc(-1 * var(--w)) 0 white,
+      calc(-2 * var(--w)) -10px white,
+      calc(-3 * var(--w)) 0 white,
+      calc(-4 * var(--w)) 0 white,
+      calc(-5 * var(--w)) 0 white,
+      calc(-6 * var(--w)) 0 white,
+      calc(-7 * var(--w)) 0 white,
+      calc(-8 * var(--w)) 0 white,
+      calc(-9 * var(--w)) 0 white;
+  }
+  36.36% {
+    text-shadow:
+      calc(0 * var(--w)) 0 white,
+      calc(-1 * var(--w)) 0 white,
+      calc(-2 * var(--w)) 0 white,
+      calc(-3 * var(--w)) -10px white,
+      calc(-4 * var(--w)) 0 white,
+      calc(-5 * var(--w)) 0 white,
+      calc(-6 * var(--w)) 0 white,
+      calc(-7 * var(--w)) 0 white,
+      calc(-8 * var(--w)) 0 white,
+      calc(-9 * var(--w)) 0 white;
+  }
+  45.45% {
+    text-shadow:
+      calc(0 * var(--w)) 0 white,
+      calc(-1 * var(--w)) 0 white,
+      calc(-2 * var(--w)) 0 white,
+      calc(-3 * var(--w)) 0 white,
+      calc(-4 * var(--w)) -10px white,
+      calc(-5 * var(--w)) 0 white,
+      calc(-6 * var(--w)) 0 white,
+      calc(-7 * var(--w)) 0 white,
+      calc(-8 * var(--w)) 0 white,
+      calc(-9 * var(--w)) 0 white;
+  }
+  54.54% {
+    text-shadow:
+      calc(0 * var(--w)) 0 white,
+      calc(-1 * var(--w)) 0 white,
+      calc(-2 * var(--w)) 0 white,
+      calc(-3 * var(--w)) 0 white,
+      calc(-4 * var(--w)) 0 white,
+      calc(-5 * var(--w)) -10px white,
+      calc(-6 * var(--w)) 0 white,
+      calc(-7 * var(--w)) 0 white,
+      calc(-8 * var(--w)) 0 white,
+      calc(-9 * var(--w)) 0 white;
+  }
+  63.63% {
+    text-shadow:
+      calc(0 * var(--w)) 0 white,
+      calc(-1 * var(--w)) 0 white,
+      calc(-2 * var(--w)) 0 white,
+      calc(-3 * var(--w)) 0 white,
+      calc(-4 * var(--w)) 0 white,
+      calc(-5 * var(--w)) 0 white,
+      calc(-6 * var(--w)) -10px white,
+      calc(-7 * var(--w)) 0 white,
+      calc(-8 * var(--w)) 0 white,
+      calc(-9 * var(--w)) 0 white;
+  }
+  72.72% {
+    text-shadow:
+      calc(0 * var(--w)) 0 white,
+      calc(-1 * var(--w)) 0 white,
+      calc(-2 * var(--w)) 0 white,
+      calc(-3 * var(--w)) 0 white,
+      calc(-4 * var(--w)) 0 white,
+      calc(-5 * var(--w)) 0 white,
+      calc(-6 * var(--w)) 0 white,
+      calc(-7 * var(--w)) -10px white,
+      calc(-8 * var(--w)) 0 white,
+      calc(-9 * var(--w)) 0 white;
+  }
+  81.81% {
+    text-shadow:
+      calc(0 * var(--w)) 0 white,
+      calc(-1 * var(--w)) 0 white,
+      calc(-2 * var(--w)) 0 white,
+      calc(-3 * var(--w)) 0 white,
+      calc(-4 * var(--w)) 0 white,
+      calc(-5 * var(--w)) 0 white,
+      calc(-6 * var(--w)) 0 white,
+      calc(-7 * var(--w)) 0 white,
+      calc(-8 * var(--w)) -10px white,
+      calc(-9 * var(--w)) 0 white;
+  }
+  90.90% {
+    text-shadow:
+      calc(0 * var(--w)) 0 white,
+      calc(-1 * var(--w)) 0 white,
+      calc(-2 * var(--w)) 0 white,
+      calc(-3 * var(--w)) 0 white,
+      calc(-4 * var(--w)) 0 white,
+      calc(-5 * var(--w)) 0 white,
+      calc(-6 * var(--w)) 0 white,
+      calc(-7 * var(--w)) 0 white,
+      calc(-8 * var(--w)) 0 white,
+      calc(-9 * var(--w)) -10px white;
+  }
+}
+</style>
